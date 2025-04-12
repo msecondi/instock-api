53c46e01f1a465bcbaf9884dc2c495edfde6aae3
matt secondi <matt_secondi@hotmail.com>
2025-04-12 11:48:25 -0400

    added warehouse class to help make validation modular

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -4,125 +4,6 @@ const knex = initKnex(configuration);
 
 import Warehouse from "../model/warehouse.js"; // Warehouse class for validation
 
-//declare functions for validation
-function hasKey(keyName, object) {
-    const objPair = Object.entries(object); //creates an array of arrays with key, value pairs
-    //check if object contains 'keyName' as a key and return it
-    return objPair.find( element => element[0] === keyName);
-}
-    
-function validPhoneNumber(contactPhoneNo) {
-    
-    //parse to string if user entered int
-    if(typeof contactPhoneNo === 'number') {
-        contactPhoneNo = contactPhoneNo.toString();
-    }
-    //use regex to ensure phone number is flexible on formatting
-    if(!contactPhoneNo.match(/^\+?\d*\s?\(?\d{3}\)?\s?\d{3}[\s-]?\w?\d{4}$/)){
-        return {
-            validPhoneNo: false
-        }
-    }
-    return {
-        validPhoneNo: true
-    }
-}
-
-function validEmailAddress(contactEmail) {
-    //use regex to ensure proper email formatting is used by user
-    if(!contactEmail.match(/^[\w.-]+@[\w.-]+\.\w+$/)) {
-        return {
-            validEmail: false
-        }
-    }
-    return {
-        validEmail: true
-    }
-}
-
-function validWarehouse(obj) {
-    //delcare an array containing all required object keys to create a warehouse with
-    const objKeys = ['warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_position', 'contact_phone', 'contact_email'];
-    
-    //gather keys requested by user
-    const requestedObjKeys = Object.keys(obj);
-
-    //declare array to capture all keys that are missing to later display to user
-    // let missingKeysArr = []; 
-    let missingKeysArr = objKeys.filter(key => !requestedObjKeys.includes(key));
-    console.log(missingKeysArr);
-    //iterate through required keys array and ensure requested object includes each one. 
-    // for(let i = 0; i < objKeys.length; i++) {
-    //     if(!requestedObjKeys.includes(objKeys[i])){
-    //         //add missing key to array
-    //         missingKeysArr.push(objKeys[i]);
-    //     }
-    // }
-    console.log("missingkeysArr: " + missingKeysArr);
-    //if any keys are missing, return object displaying input not valid, with all missing values
-    if(missingKeysArr.length) {
-        return {
-            validKeys: false,
-            missingKeys: missingKeysArr
-        };
-    }
-
-    //gather key/value pairs requested by user
-    const requestedObj = Object.entries(obj);
-
-    //loop through object and ensure all values are 'non-empty'
-    const emptyValues = [];
-    requestedObj.forEach((value) => {
-        // Specify 'falsy' values to allow user to include 0 as a valid value
-        if (value[1] === null || value[1] === undefined || (typeof value[1] === 'string' && value[1].trim() === "")) {
-            emptyValues.push(value);
-        }
-    })
-    console.log("emptyValuesArr: " + emptyValues);
-    //if there are values within 'emptyValues', return 'validValues: false' and pass array with all values that need correction
-    if(emptyValues.length) {
-        return {
-            validKeys: true,
-            validValues: false,
-            missingValues: emptyValues
-        };
-    }
-    
-    //ADD PHONE & EMAIL VALIDATION FUNCTIONS
-    //since req.body has been validated for correct keys & values, pass corresponding value to be validated for formatting
-    //validate 'contact_phone' key
-    let properFormat = validPhoneNumber(obj.contact_phone);
-    console.log(properFormat);
-    if(!properFormat.validPhoneNo) {
-        return {
-            validKeys: true,
-            validValues: true,
-            phoneFormat: false
-        }
-    }
-
-    //validate 'contact_email' key
-    properFormat = validEmailAddress(obj.contact_email);
-    console.log(properFormat);
-    if(properFormat.validPhoneNo) {
-        return {
-            validKeys: true,
-            validValues: true,
-            phoneFormat: true,
-            emailFormat: false
-        }
-    }
-
-    //passed all validation, return all true
-    return { 
-        validKeys: true,
-        validValues: true,
-        phoneFormat: true,
-        emailFormat: true
-    };
-}
-
-
 const allWarehouses = async(req, res) => {
     try {
         const data = await knex('warehouses');
@@ -133,21 +14,32 @@ const allWarehouses = async(req, res) => {
 }
 
 const createWarehouse = async(req, res) => {
-    // const result = validWarehouse(req.body);
-    // console.log(result)
-    // // console.log(req.body.contact_phone)
-    // if(!result.validKeys){
-    //     res.send(`Keys missing from requested warehouse: ${result.missingKeys.map(e => ` '${e}'`)}`);
-    // }
-    // else if(!result.validValues){
-    //     res.send(`Values missing from requested warehouse key: '${result.missingValues.map(e => `{ ${e[0]}: '${e[1]}' }`)}'`);
-    // }
-    const requestedWarehouse = new Warehouse(req.body);
-    const valid = requestedWarehouse.validWarehouse();
-    if(valid.errorMessage) {
-        return res.send(valid.errorMessage);
+    try{
+        //VALIDATION START
+        //create an instance of Warehouse to gain access to class
+        const requestedWarehouse = new Warehouse(req.body);
+        //use the validation function within the Warehouse class to validate requested warehouse object and assign to variable
+        const validationResult = requestedWarehouse.validateWarehouse('create'); //specifying which validation required
+        //check to see if the response from validation passed an error message. If so, return it and display to user
+        if(validationResult.errorMessage) {
+            return res.status(422).send(validationResult.errorMessage);
+        }
+        //VALIDATION END
+
+        //passed validation, now 'create' warehouse
+        const data = await knex('warehouses').insert(req.body);
+        // assign new id to variable
+        const newWarehouseId = data[0];
+        //now retrieve new warehouse information and send as a response
+        const newWarehouse = await knex('warehouses')
+            .select('id', 'warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_position', 'contact_phone', 'contact_email')
+            .where({ id: newWarehouseId }) // use new id to select new warehouse
+            .first(); // instead of using newWarehouse[0]
+    
+        res.status(200).send(newWarehouse);
+    } catch (error) {
+        res.status(400).send(`Error creating warehouse: ${error}`);
     }
-    res.send("You did it!")
 }
 
 const oneWarehouse = async(req, res) => {
@@ -182,51 +74,17 @@ const inventoryInWarehouse = async(req, res) => {
 const updateWarehouse = async(req, res) => {
     try {
         //VALIDATION START
-        //declare variables used to validate input
-        let isValid = true;
-        let index = 0; 
-        
-        const objPair = Object.entries(req.body); //creates an array of arrays with key, value pairs
-
-        //check if requested update includes 'contact_phone' and 'contact_email'
-        const hasPhoneNumber = objPair.find( element => element[0] === "contact_phone");
-        const hasEmail = objPair.find( element => element[0] === "contact_email");
-        
-        //if 'contact_phone' key found, check if it is a valid phone number --> 10 digits, non negative
-        if(hasPhoneNumber) {
-            //parse to string if user entered int
-            if(typeof hasPhoneNumber[1] === 'number') {
-                hasPhoneNumber[1] = hasPhoneNumber[1].toString();
-            }
-            //use regex to ensure phone number is flexible on formatting
-            hasPhoneNumber[1].match(/^\+?\d*\s?\(?\d{3}\)?\s?\d{3}[\s-]?\w?\d{4}$/) ? isValid : isValid = false;
-            if (!isValid) {
-                return res.status(400).send("Not a valid phone number.") 
-            }
-        }
-        //if 'contact_email' key found, check if it is a valid email '@ & .'
-        if(hasEmail) {
-            //use regex to ensure proper email formatting is used by user
-            hasEmail[1].match(/^[\w.-]+@[\w.-]+\.\w+$/) ? isValid : isValid = false;
-            if (!isValid) {
-                return res.status(400).send("Not valid email. Please ensure email includes '.' and '@'.");  
-            }
-        }
-        //loop through object and ensure all values are 'non-empty'
-        const missingValue = objPair.some((value, i) => {
-            // Specify 'falsy' values to allow user to include 0 as a valid value
-            if (value[1] === null || value[1] === undefined || (typeof value[1] === 'string' && value[1].trim() === "")) {
-                index = i; // capture index to display more detailed error message 
-                return true; //missingValue = true
-            }
-        })
-        //if missingValue is true, req.body contains a 'falsy' value, therefore abort and send '409 - Conflict' error
-        if (missingValue) {
-            return res.status(409).send(`Requested data FAILED to update due to empty value --> { ${objPair[index][0]}: '${objPair[index][1]}' }`); 
+        //create an instance of Warehouse to gain access to class
+        const requestedWarehouse = new Warehouse(req.body);
+        //use the validation function within the Warehouse class to validate requested warehouse object and assign to variable
+        const validationResult = requestedWarehouse.validateWarehouse('update'); //specifying which validation required
+        //check to see if the response from validation passed an error message. If so, return it and display to user
+        if(validationResult.errorMessage) {
+            return res.status(422).send(validationResult.errorMessage);
         }
         //VALIDATION END
 
-        // select desired table and update with validated data
+        //select desired table and update with validated data
         const selectedWarehouse = await knex('warehouses')
             .where({id: req.params.warehouseId })
             .update(req.body);
--- model/warehouse.js
+++ model/warehouse.js
@@ -1,7 +1,7 @@
 class Warehouse {
     constructor(obj) {
-        this.obj = obj;
-        this.requiredKeys = [
+        this.obj = obj; //give instance of class access to user inputted obj data
+        this.requiredKeys = [ //initialize array to reference all required keys when creating a warehouse
             'warehouse_name',
             'address',
             'city',
@@ -15,6 +15,9 @@ class Warehouse {
 
     //check phone number formatting function (returns null if it doesn't match)
     validPhoneNo(phoneNo) {
+        if(typeof phoneNo === 'number') { //'defensive'
+            phoneNo = phoneNo.toString();
+        }
         //use regex to ensure phone number is flexible on formatting
         return phoneNo.match(/^\+?\d*\s?\(?\d{3}\)?\s?\d{3}[\s-]?\d{4}$/);
     }
@@ -25,22 +28,9 @@ class Warehouse {
         return email.match(/^[\w.-]+@[\w.-]+\.\w+$/);
     }
 
-    validWarehouse() {
-        //instantiated array that gives all keys requested by user
-        const requestedKeys = Object.keys(this.obj);
-        
-        //filter through all required keys. Assign any missing to an array, to be returned as an error message
-        const missingKeys = this.requiredKeys.filter(key => !requestedKeys.includes(key));
-        //if any keys are missing, return object displaying input not valid, with all missing keys
-        if(missingKeys.length) {
-            return {
-                validKeys: false,
-                invalidKeys: missingKeys,
-                errorMessage: `Keys missing from requested warehouse: ${missingKeys.map(e => ` '${e}'`)}`
-            };
-        }
-
-        //create an array of 'emptyValues' if any values are empty
+    //check to see if any values are empty
+    checkEmptyValues() {
+        //filter through object and create an array of 'emptyValues' if any values are empty
         const emptyValues = Object.entries(this.obj).filter(
             value => value[1] === null ||
             value[1] === undefined || 
@@ -49,39 +39,89 @@ class Warehouse {
         //if any values are missing, return object displaying input not valid, with all missing values
         if(emptyValues.length) {
             return {
-                validKeys: true,
-                validValues: false,
-                invalidValues: emptyValues,
                 errorMessage: `Values missing from requested warehouse: '${emptyValues.map(e => `{ ${e[0]}: '${e[1]}' }`)}`
             };
         }
-        //if user entered 'contact_phone' doesn't match desired format, return an error
-        if(!this.validPhoneNo(this.obj.contact_phone)){
-            return {
-                validKeys: true,
-                validValues: true,
-                phoneFormat: false,
-                errorMessage: `${this.obj.contact_phone} is not a valid phone number format.`
-            }
+        return {
+            success: true
         }
-        //if user entered 'contact_email' doesn't match desired format, return an error
-        if(!this.validEmail(this.obj.contact_email)){
-            return {
-                validKeys: true,
-                validValues: true,
-                phoneFormat: true,
-                emailFormat: false,
-                errorMessage: `${this.obj.contact_email} is not a valid email format.`
-            }
-        }
+    }
 
-        //passed all validation, return all true
-        return { 
-            validKeys: true,
-            validValues: true,
-            phoneFormat: true,
-            emailFormat: true
-        };
+    validateWarehouse(crudType) {
+        //check what type of validation is required based on paramter
+        if(crudType === 'create') { //create NEW warehouse
+            //instantiated array that gives all keys requested by user
+            const requestedKeys = Object.keys(this.obj);
+            
+            //filter through all required keys. Assign any missing to an array, to be returned as an error message
+            const missingKeys = this.requiredKeys.filter(key => !requestedKeys.includes(key));
+            //if any keys are missing, return object displaying input not valid, with all missing keys
+            if(missingKeys.length) {
+                return {
+                    errorMessage: `Keys missing from requested warehouse: ${missingKeys.map(e => ` '${e}'`)}`
+                };
+            }
+            // check if any values are empty using 'checkEmptyValues' class method
+            const hasEmptyValues = this.checkEmptyValues();
+            //if object returned includes 'errorMessage' key, return it
+            if(hasEmptyValues.errorMessage) {
+                return {
+                    errorMessage: hasEmptyValues.errorMessage
+                };
+            }
+            //if 'contact_phone' doesn't match desired format, return an error
+            if(!this.validPhoneNo(this.obj.contact_phone)){
+                return {
+                    errorMessage: `ERROR  '${this.obj.contact_phone}' is not a valid phone number format.`
+                }
+            }
+            //if 'contact_email' doesn't match desired format, return an error
+            if(!this.validEmail(this.obj.contact_email)){
+                return {
+                    errorMessage: `ERROR  '${this.obj.contact_email}' is not a valid email format.`
+                }
+            }
+            //passed all validation, return true
+            return { 
+                success: true
+            };
+        }
+        else if(crudType === 'update') { //update EXISTING warehouse
+            // check if any values are empty using 'checkEmptyValues' class method
+            const hasEmptyValues = this.checkEmptyValues();
+            //if object returned includes 'errorMessage' key, return it
+            if(hasEmptyValues.errorMessage) {
+                return {
+                    errorMessage: hasEmptyValues.errorMessage
+                };
+            }
+
+            //if user entered 'contact_phone' & 'contact_email' keys, assign to variables
+            const phoneNo = this.obj.contact_phone;
+            const email = this.obj.contact_email;
+
+            //if user entered 'contact_phone' & it doesn't match desired format, return an error
+            if (phoneNo && !this.validPhoneNo(phoneNo)) {
+                return {
+                    errorMessage: `ERROR '${phoneNo}' is not a valid phone number format.`
+                }
+            }
+            //if user entered 'contact_email' & it doesn't match desired format, return an error
+            if (email && !this.validEmail(email)) {
+                return {
+                    errorMessage: `ERROR '${email}' is not a valid email format.`
+                }
+            }
+            //passed all validation, return true
+            return { 
+                success: true
+            };
+        }
+        else { //did not specify 'create' or 'update'
+            return {
+              errorMessage: `Invalid CRUD type '${crudType}'. Must use 'create' or 'update'.`
+            }
+        }
     }
 }
 
